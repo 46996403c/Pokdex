37e6c3e9c1f0e9adac0aefb67cb189a0d6ed34d4
oscar <46996403c@iespoblenou.org>
2016-01-02 12:03:33 +0100

    Agregos las ID a los elementos e inserto los primeros 151 pokemons de la primera generacion

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,11 +3,34 @@ package sample;
 import javafx.application.Platform;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 
 import java.util.Optional;
 
 public class Controller {
     public ImageView IVsprite;
+    public Button BTinfo;
+    public Button BTapagar;
+    public Button BTrandom;
+    public TextField TFescribirPokemon;
+    public MenuButton MBseleccionarPokemon;
+    public GridPane GPpropiedades;
+    public Text TnombreFijo;
+    public Text TpesoFijo;
+    public Text TalturaFijo;
+    public Text TtipoFijo;
+    public Text TataquePrincipalFijo;
+    public Text TnombreVariable;
+    public Text TpesoVariable;
+    public Text TalturaVariable;
+    public Text TtipoVariable;
+    public Text TataquePrincipalVariable;
+    public ScrollBar SBzoom;
+    public Button BTbuscar;
+    public Button BTreset;
+
+
     public void Exit(){
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Apagar Pokedex");
@@ -24,5 +47,18 @@ public class Controller {
         alert.setHeaderText("Look, an Information Dialog");
         alert.setContentText("I have a great message for you!");
         alert.showAndWait();
+    }
+    public void Reset(){
+        TFescribirPokemon.clear();
+        TnombreVariable.setText("");
+        TpesoVariable.setText("");
+        TalturaVariable.setText("");
+        TtipoVariable.setText("");
+        TataquePrincipalVariable.setText("");
+        MBseleccionarPokemon.setText("Selecciona un Pokemon");
+    }
+    public void prueba (){
+        MBseleccionarPokemon.setText("Hola");
+        TFescribirPokemon.setText("Hola");
     }
 }
